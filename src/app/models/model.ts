c7d276111d43accdbcf6a9d8af09d9cbabd5774c
huynhgiahuy0409 <76533370+huynhgiahuy0409@users.noreply.github.com>
2023-02-16 12:45:54 +0700

    footer

---
+++ src/app/models/model.ts
@@ -0,0 +1,4 @@
+export interface RedirectInfo{
+    label: string,
+    path: string
+}
--- src/app/shared/layout/customer-layout/customer-footer/customer-footer.component.ts
+++ src/app/shared/layout/customer-layout/customer-footer/customer-footer.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { RedirectInfo } from 'src/app/models/model';
+
 
 @Component({
   selector: 'app-customer-footer',
@@ -6,5 +8,58 @@ import { Component } from '@angular/core';
   styleUrls: ['./customer-footer.component.scss']
 })
 export class CustomerFooterComponent {
-
+  policies: RedirectInfo[] = [
+    {
+      label: 'Giới thiệu về Mioto',
+      path: ''
+    },
+    {
+      label: 'Chính sách và quy định',
+      path: ''
+    },
+    {
+      label: 'Quy chế hoạt động',
+      path: ''
+    },
+    {
+      label: 'Bảo mật thông tin',
+      path: ''
+    },
+    {
+      label: 'Giải quyết tranh chấp',
+      path: ''
+    },
+  ]
+  infoMores: RedirectInfo[] = [
+    {
+      label: 'Hướng dẫn chung',
+      path: ''
+    },
+    {
+      label: 'Hướng dẫn đặt xe',
+      path: ''
+    },
+    {
+      label: 'Hướng dẫn dành cho chủ xe',
+      path: ''
+    },
+    {
+      label: 'Hướng dẫn thanh toán',
+      path: ''
+    },
+    {
+      label: 'Tuyển dụng',
+      path: ''
+    },
+  ]
+  partners: RedirectInfo[] = [
+    {
+      label: 'Đăng ký chủ xe Mioto',
+      path: ''
+    },
+    {
+      label: 'Đăng ký Bảo hiểm vật chất & Bảo hiểm TNDS xe ô tô',
+      path: ''
+    },
+  ]
 }
