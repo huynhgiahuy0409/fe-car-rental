31aa5299be5fb73f9892786c4a9a597ef6cfcd43
huynhgiahuy0409 <76533370+huynhgiahuy0409@users.noreply.github.com>
2023-02-24 11:35:08 +0700

    complete customize carousel  and refactor old carousel

--- src/app/customer/components/home/components/prominent-car/prominent-car.component.ts
+++ src/app/customer/components/home/components/prominent-car/prominent-car.component.ts
@@ -7,7 +7,6 @@ import { Component } from '@angular/core';
 })
 export class ProminentCarComponent {
   categories: number[] = [
-    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
     1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1
   ];
 }
--- src/app/customer/components/home/components/prominent-place/prominent-place.component.ts
+++ src/app/customer/components/home/components/prominent-place/prominent-place.component.ts
@@ -1,4 +1,15 @@
-import { Component, ElementRef, EventEmitter, Input, Output, QueryList, SimpleChanges, ViewChild, ViewChildren } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  Input,
+  Output,
+  QueryList,
+  SimpleChanges,
+  ViewChild,
+  ViewChildren,
+} from '@angular/core';
+import { CarouselComponent } from 'src/app/shared/carousel/carousel.component';
 
 @Component({
   selector: 'app-prominent-place',
@@ -6,9 +17,40 @@ import { Component, ElementRef, EventEmitter, Input, Output, QueryList, SimpleCh
   styleUrls: ['./prominent-place.component.scss'],
 })
 export class ProminentPlaceComponent {
-  categories: number[] = [
-    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1
-  ];
-   
+  @Input()
+  primaryClr!: string;
+  @Input()
+  secondaryClr!: string;
+  @Input()
+  tertiaryClr!: string;
+  activeDot!: any
+  unActiveDot!:any
+  categories: number[] = [1,2,3,4,5,6,7,8];
+  categories1: number[] = [1,2,3,4,5,6,7,8, 9, 10, 11,12,13,14,15,16, 17];
+  @ViewChild('airPortCarousel')
+  airPortCarousel!: CarouselComponent
+  constructor() {
+  }
+  ngAfterViewInit(){
+    this.activeDot = {
+      backgroundColor: this.primaryClr,
+      border: `2px solid ${this.secondaryClr}`,
+    };
+    this.unActiveDot = {
+      backgroundColor: this.primaryClr,
+      opacity: 0.2,
+    };
+    console.log(this.activeDot);
+    console.log(this.unActiveDot);
+    
+  }
+  setDestinationSltBgClr(): string {
+    return `linear-gradient(180deg,#fff 0,#fff 54%,${this.tertiaryClr} 0,${this.tertiaryClr})`;
+  }
+  setImageBefore(): string {
+    return `linear-gradient(transparent,${this.primaryClr})`;
+  }
+  sltDot(idx: number){
+    this.airPortCarousel.processDots(idx)
+  }
 }
