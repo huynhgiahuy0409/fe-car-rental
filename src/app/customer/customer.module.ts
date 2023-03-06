79d2960304d282ed676b99756541509b7788da7f
huynhgiahuy0409 <76533370+huynhgiahuy0409@users.noreply.github.com>
2023-03-06 18:25:35 +0700

    customize material + reuse  Roboto Font

--- src/app/customer/customer.module.ts
+++ src/app/customer/customer.module.ts
@@ -5,6 +5,7 @@ import { CustomerLayoutModule } from '../shared/layout/customer-layout/customer-
 import { CustomerRoutingModule } from './customer-routing.module';
 import { CustomerComponent } from './customer.component';
 import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { MaterialAngularModule } from '../material-angular/material-angular.module';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { SignUpComponent } from './components/sign-up/sign-up.component';
     CommonModule,
     CustomerRoutingModule,
     CustomerLayoutModule,
+    MaterialAngularModule
   ]
 })
 export class CustomerModule { }
