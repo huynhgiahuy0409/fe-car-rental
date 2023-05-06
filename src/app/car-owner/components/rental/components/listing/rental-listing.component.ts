6ea98f9bf1c42ce63fece170b571ba25f0c330d4
Hieu Nguyen <nhocproaoe@gmail.com>
2023-05-06 22:42:07 +0700

    refactor: add sort list in rental listing page

--- src/app/car-owner/components/rental/components/listing/rental-listing.component.ts
+++ src/app/car-owner/components/rental/components/listing/rental-listing.component.ts
@@ -5,6 +5,7 @@ import vi from 'date-fns/locale/vi';
 import { CarOwnerService } from 'src/app/car-owner/services/car-owner.service';
 import { CAR_IMG } from 'src/app/models/constance';
 import { RentalListingResponse } from 'src/app/models/response/model';
+import { getMoneyFormat } from 'src/app/shared/utils/MoneyUtils';
 
 @Component({
   selector: 'app-rental-listing',
@@ -12,17 +13,13 @@ import { RentalListingResponse } from 'src/app/models/response/model';
   styleUrls: ['./rental-listing.component.scss']
 })
 export class RentalListingComponent {
-  now: any;
   locale: any;
-  end: any;
   rentalList: RentalListingResponse[] = [];
+  sortedRentalList: RentalListingResponse[] = [];
   readonly BASE_IMG = CAR_IMG;
 
   constructor(private _formBuilder: FormBuilder, private carService: CarOwnerService) {
     this.locale = { vi: vi }
-    // this.now = format(new Date(), "eeee - dd/mm/yyyy hh:mm", { locale: this.locale.vi }); // with day of week
-    this.now = format(new Date(), "dd/MM/yyyy hh:mm", { locale: this.locale.vi });
-    this.end = format(new Date("2023/03/07"), "dd/MM/yyyy hh:mm", { locale: this.locale.vi });
   }
 
   rentalListSortFormGroup = this._formBuilder.group({
@@ -32,6 +29,20 @@ export class RentalListingComponent {
   ngOnInit(): void {
     this.carService.getRentalListByOwner("hieu").subscribe(res => {
       this.rentalList = res;
+      this.sortedRentalList = res;
+    });
+
+    this.rentalListSortFormGroup.get('orderBy')?.valueChanges.subscribe(value => {
+      console.log("changed");
+
+      if (Number(value) < 1)
+        this.sortedRentalList = this.sortedRentalList.sort((a, b) => {
+          return b.startDate - a.startDate;
+        });
+      else
+        this.sortedRentalList = this.sortedRentalList.sort((a, b) => {
+          return a.startDate - b.startDate;
+        });
     });
   }
 
@@ -39,4 +50,7 @@ export class RentalListingComponent {
     return format(new Date(date), "dd/MM/yyyy hh:mm", { locale: this.locale.vi });
   }
 
+  getFormattedMoney(money: number) {
+    return getMoneyFormat(money);
+  }
 }
